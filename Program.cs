f0c6ad081b3ebb076c6a7d9c611e3267e6ba1f47
SkillFactoryCSharpTest <dashulichka311214@gmail.com>
2024-01-23 16:16:26 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Dasha";
+            byte MyAge = 18;
+            bool HaveIApet = true;
+            double MyShoeSize = 36;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("MyAge " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+        }
+    }
+}
